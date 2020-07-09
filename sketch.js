19196148d1071f34f6103eb57627572bfc0f0bf0
Ani21gul <66204810+Ani21gul@users.noreply.github.com>
2020-07-09 16:36:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+var paper;
+var leftSide, rightSide, bottomSide;
+var ground;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1350, 550);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	paper = new Paper(200,450,40);
+	
+	
+
+	leftSide = new Sides(1085,450,20,150);
+	rightSide = new Sides(1215,450,20,150);
+	bottomSide = new Sides(1150,530,150,20);
+
+
+	//packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
+	//World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = new Ground(width/2, 545, width, 10);
+ 
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  fill("red");
+  text(mouseX,50,50);
+  text(mouseY,50,70);
+  text("PRESS SPACE TO PUT THE PAPER IN THE DUSTBIN",500,100)
+
+  paper.display();
+  leftSide.display();
+  rightSide.display();
+  bottomSide.display();
+  ground.display();
+  
+  
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyCode === 32){
+        
+		Matter.Body.applyForce(paper.body, paper.body.position,{x:320, y:-400});
+	}
+}
+
+
+
